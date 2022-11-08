499cb858d4a89f475e0cd102902e44d89359039c
Gingerdasha <117080444+Gingerdasha@users.noreply.github.com>
2022-11-08 13:54:30 +0200

    weather icon js
    dynamic weather icon added

--- src/index.js
+++ src/index.js
@@ -38,6 +38,12 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon-name")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
 function searchCity(city) {
