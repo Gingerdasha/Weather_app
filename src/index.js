7fe51f5f27808ea01a788d4717821b14d2d15560
Gingerdasha <117080444+Gingerdasha@users.noreply.github.com>
2022-11-16 00:53:10 +0200

    final touches 😜

--- src/index.js
+++ src/index.js
@@ -32,7 +32,6 @@ function formatDay(timestamp) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "caa883a4a60d93878755b08a933f74ea";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -74,7 +73,6 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 //
 
